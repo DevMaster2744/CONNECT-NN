f135affffebfce8f54c067d6fab556a141451e8f
DevMaster2744 <od.pedromega@gmail.com>
2024-07-04 11:56:24 -0300

    Neural Network - Frist Version - 1.0

--- connectnn.py
+++ connectnn.py
@@ -8,7 +8,7 @@ class activationFunction(Enum):
     BINARY = 2
     TANH = 3
 
-def activationFunctionFromEnum(x: float, activationFunctionType: activationFunction):
+def activationFunctionFromEnum(x: float, activationFunctionType: activationFunction) -> float:
     if activationFunctionType == activationFunction.SIGMOID:
         return 1 / (1 + np.exp(-x))
     
@@ -22,20 +22,59 @@ def activationFunctionFromEnum(x: float, activationFunctionType: activationFunct
 
 class unit():
     def __init__(self, inputSize: int, activationFunctionType: activationFunction) -> None:
-        
         self.heights = [uniform(-1,1) for _ in range(inputSize)]
         self.activationFunctionType = activationFunctionType
+        self.inputs = []
+        self.output = 0
+    def run(self, inputs: tuple) -> float:
+        self.inputs = inputs
+
+        dot = np.dot(inputs, self.heights)
+        
+        result = np.sum(dot)
+
+        result = activationFunctionFromEnum(result, self.activationFunctionType)
+        return result
+
+    def fit(self, error: float, alpha: float):
+        for i in range(len(self.heights)):
+            self.heights[i] +=  activationFunctionFromEnum(self.inputs[i], activationFunction.TANH) * error * alpha
+    '''def setInputSize(self, inputSize: int):
+        self.inp'''
+
 
 class layer():
     def __init__(self, inputSize: int, layerSize: int, activationFunctionType: activationFunction) -> None:
         self.units = [unit(inputSize=inputSize, activationFunctionType=activationFunctionType) for _ in range(layerSize)]
-
+    def fit(self, error: float, alpha: float):
+        for unit in self.units:
+            unit.fit(error, alpha)
+    def run(self, inputs: tuple):
+        result = []
+        for unit in self.units:
+            result.append(unit.run(inputs))
+        return result
+    def _get_units_len_(self):
+        return len(self.units)
 
 class NeuralNetwork():
     def __init__(self, initialInputSize: int) -> None:
         self.initialInputSize = initialInputSize
         self.layers = []
-
-    def addLayer(self, layerSize: int, layerType: activationFunction):
-        self.layers.append(layer(self.initialInputSize, layerSize, layerType))
+    def addLayer(self, layerSize: int, layerType: activationFunction) -> None:
+        if len(self.layers) == 0:
+            self.layers.append(layer(self.initialInputSize, layerSize, layerType))
+        else:
+            self.layers.append(layer(self.layers[-1]._get_units_len_(), layerSize, layerType))
+    def fit(self, error: float, alpha: float):
+        for layer in self.layers:
+            layer.fit(error, alpha)
+    def run(self, inputs: tuple):
+        result = inputs
+        for i, layer in enumerate(self.layers):
+            result = layer.run(result)
+            print(result)
         
+        return result
+
+    
--- test.py
+++ test.py
@@ -1,4 +1,13 @@
 import connectnn as cn
+from time import sleep
+import random
 
 nn = cn.NeuralNetwork(2)
+
+nn.addLayer(35, cn.activationFunction.TANH)
+
+nn.addLayer(40, cn.activationFunction.TANH)
+
 nn.addLayer(5, cn.activationFunction.TANH)
+
+nn.addLayer(1, cn.activationFunction.SIGMOID)
