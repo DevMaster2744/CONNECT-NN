0ba8e37450407e1df987b72bf7175814b37f2812
odthiago <od.pedromega@gmail.com>
2024-11-23 16:07:00 -0300

    setup updated

--- setup.bat
+++ setup.bat
@@ -31,11 +31,11 @@ echo Checking %~1
 .\.venv\Scripts\python.exe -c "import %~1" 2>nul
 
 if %errorlevel% neq 0 (
-    echo "Installing %~1"
+    echo Installing %~1
     .\.venv\Scripts\pip.exe install %~1
     if %errorlevel% neq 0 (
-        echo "Error when installing %~1, you may need to check it manualy."
-        echo "Aborting execution"
+        echo Error when installing %~1, you may need to check it manualy.
+        echo Aborting execution
         EXIT /B 1
         exit /b
     ) else (
